2864b7e7ffd51bd77352618c0c8b968c7e3f8f50
IBlutman155201 <Ido_Blutman@amat.com>
2017-11-19 16:45:17 +0200

    Initial commit

---
+++ bike-pred/bike_pred.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_path = 'Bike-Sharing-Dataset/hour.csv'
+
+rides = pd.read_csv(data_path)
+rides[:24*10].plot(x='dteday', y='cnt')
+
+# Convert categorical variables like season, weather, month to dummy variables
+dummy_fields = ['season', 'weathersit', 'mnth', 'hr', 'weekday']
+for each in dummy_fields:
+    dummies = pd.get_dummies(rides[each], prefix=each, drop_first=False)
+    rides = pd.concat([rides, dummies], axis=1)
+
+# Remove unused columns
+fields_to_drop = ['instant', 'dteday', 'season', 'weathersit',
+                  'weekday', 'atemp', 'mnth', 'workingday', 'hr']
+data = rides.drop(labels=fields_to_drop, axis=1)
+
+# To make training the network easier, we'll standardize each of the continuous variables.
+# That is, we'll shift and scale the variables such that they have zero mean and a standard deviation of 1.
+# The scaling factors are saved so we can go backwards when we use the network for predictions.
+quant_features = ['casual', 'registered', 'cnt', 'temp', 'hum', 'windspeed']
+# Store scalings in a dictionary so we can convert back later
+scaled_features = {}
+for each in quant_features:
+    mean, std = data[each].mean(), data[each].std()
+    scaled_features[each] = [mean, std]
+    data.loc[:, each] = (data[each] - mean)/std
+
+
+
